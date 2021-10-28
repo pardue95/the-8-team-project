6f0a57858a41a1de805b8db1bc819738b384fcb9
pardue95 <cwpardue@gmail.com>
2021-10-27 21:28:45 -0500

    console.log is pulling data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+var cocktailFormEl = document.querySelector('#cocktail-form');
+var cocktailInputEl = document.querySelector('#cocktail-name');
+var cocktailContainerEl = document.querySelector('#cocktail-container');
+var cocktailSearchTerm = document.querySelector('#cocktail-search-term');
+console.log(cocktailFormEl)
+
 var getCocktail = function(cocktail) {
  
         // format the cocktaildb api url
-        var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+        var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktail
       
         // make a get request to url
         fetch(apiUrl)
@@ -21,5 +27,25 @@ var getCocktail = function(cocktail) {
             alert('Unable to connect to The Cocktail DB');
           });
       };
+
+      
   
-      getCocktail()
+var formSubmitHandler = function(event) {
+        // prevent page from refreshing
+        event.preventDefault();
+      
+        // get value from input element
+        var cocktailName = cocktailInputEl.value.trim();
+      
+        if (cocktailName) {
+          getCocktail(cocktailName);
+      
+          // clear old content
+          cocktailContainerEl.textContent = '';
+          cocktailInputEl.value = '';
+        } else {
+          alert('Please enter a cocktail name');
+        }
+    
+};
+cocktailFormEl.addEventListener("submit", formSubmitHandler);
