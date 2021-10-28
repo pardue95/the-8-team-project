d828bfd3370c9b2bfa2827b50a26e84eb7b370d4
pardue95 <cwpardue@gmail.com>
2021-10-28 17:00:12 -0500

    recipes are showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,8 @@ var getCocktail = function(cocktail) {
               console.log(response);
               response.json().then(function(data) {
                 console.log(data);
-                
+                console.log(data.drinks)
+                displayCocktails(data.drinks, cocktail)
               });
             } else {
               alert('Error: ' + response.statusText);
@@ -48,4 +49,29 @@ var formSubmitHandler = function(event) {
         }
     
 };
+
+var displayCocktails = function(cocktailRecipe, cocktail) {
+  //check if API returns any cocktails
+  if (cocktailRecipe.length === 0) {
+    cocktailContainerEl.textContent = "No cocktails found.";
+    return;}
+
+    cocktailSearchTerm.textContent = cocktail
+
+   for (var i = 3; i < cocktailRecipe.length; i++) {
+     //format cocktail name
+     var cocktailName =cocktailRecipe[i].strDrink
+     console.log(cocktailName)
+
+     // create a div for each cocktail
+    var cocktailEl = document.querySelector("#cocktail-container");
+    var cocktailRecipeEl = document.createElement("li");
+    cocktailRecipeEl.textContent = cocktailName;
+
+    cocktailEl.appendChild(cocktailRecipeEl)
+
+    var ingredientsEl =
+   }
+  }
+
 cocktailFormEl.addEventListener("submit", formSubmitHandler);
